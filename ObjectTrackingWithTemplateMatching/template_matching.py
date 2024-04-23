8a92dcb4439ef5eb43188b45079a7bd0da7f4a9b
Tomas Vasquez <vasquezt2015@gmail.com>
2024-04-23 15:27:06 -0600

    Final update to template matching

--- ObjectTrackingWithTemplateMatching/template_matching.py
+++ ObjectTrackingWithTemplateMatching/template_matching.py
@@ -15,21 +15,25 @@ while(True):
         ret, frame = vid.read()
         # Display the resulting frame
         cv.imshow('frame', frame)
-        # the 'q' button is set as the
-        # quitting button you may use any
-        # desired button of your choice
+        # to take a picture use "q"
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
+cv.destroyAllWindows()
 img = frame
+positions = ["first", "second", "third"]
+counter =0
 while (len(ROIs) < NUMBER_OBJECTS):
     assert img is not None, "file could not be read, check with os.path.exists()"
     #img_copy = img.copy()
     ROIs.append(cv.selectROI("select the area", img))
     cv.destroyAllWindows()
+    labels.append(input(f"Enter the label of the {positions[counter]} object: "))
+    counter +=1
 
-labels.append(input("Enter the label of the first object: "))
-labels.append(input("Enter the label of the second object: "))
-labels.append(input("Enter the label of the thrid object: "))
+
+
+#labels.append(input("Enter the label of the second object: "))
+#labels.append(input("Enter the label of the thrid object: "))
 # Crop image
 templates = [img[int(r[1]):int(r[1]+r[3]), 
                 int(r[0]):int(r[0]+r[2])] for r in ROIs]
@@ -53,23 +57,16 @@ values = []
 
 for meth in methods:
     vals = []
+    print(f"Tracking with {meth}")
     while(True):
-
         # Capture the video frame by frame
         ret, frame = vid.read()
-
-        # the 'q' button is set as the
-        # quitting button you may use any
-        # desired button of your choice
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
-
         img = frame.copy()
         method = eval(meth)
-
         # Apply template Matching
         res = [cv.matchTemplate(img,template,method) for template in templates]
-
         min_val_1, max_val_1, min_loc_1, max_loc_1 = cv.minMaxLoc(res[0])
         min_val_2, max_val_2, min_loc_2, max_loc_2 = cv.minMaxLoc(res[1])
         min_val_3, max_val_3, min_loc_3, max_loc_3 = cv.minMaxLoc(res[2])
@@ -89,21 +86,16 @@ for meth in methods:
         bottom_right_3 = (top_left_3[0] + ws[2], top_left_3[1] + hs[2])
         cv.putText(img, meth, (10, 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        # threshold = 0.8
-
-        #The lower the number the better
         if method == cv.TM_CCOEFF:
-            #threshold = 4251663
             threshold = .8
         if method == cv.TM_CCOEFF_NORMED:
             threshold = 0.677
         if method == cv.TM_CCORR:
-            #threshold = 143577735
             threshold = 0.98
         if method == cv.TM_CCORR_NORMED:
             threshold = 0.978
         if method == cv.TM_SQDIFF:
-            threshold = 0.057#3203056
+            threshold = 0.057
         if method == cv.TM_SQDIFF_NORMED:
             threshold = 0.057
 
@@ -116,25 +108,9 @@ for meth in methods:
         if (res[2][top_left_3[1]][top_left_3[0]] >= threshold):
             cv.rectangle(img,top_left_3, bottom_right_3, 255, 2)
             cv.putText(img, labels[2], (top_left_3[0], top_left_3[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv.imshow("Cropped image", img)
+        cv.imshow("Tracking", img)
         vals.append(res[0][top_left_1[1]][top_left_1[0]])
     values.append(vals)
-
-# for i in range(len(methods)):
-#     print(i)
-#     print("max:")
-#     print(max(values[i]))
-#     print("min:")
-#     print(min(values[i]))
-#     print("sum:")
-#     print(sum(values[i])/len(values[i]))
-
-# t_0 = 4251663
-# t_1 = 0.677
-# t_2 = 143577735
-# t_3 = 0.978
-# t_4 = 3203056
-# t_5 = 0.057
 
 # After the loop release the cap object
 vid.release()
